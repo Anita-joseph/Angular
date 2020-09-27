7dc4add42ceeecdbc89a5c7b667ed15eb421350c
Anita-joseph <anitarosaire@gmail.com>
2020-09-27 22:03:15 +0200

    json server fake backend initial commit

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'my-app';
+	teams$ = this.http.get('/api/teams');
+	// teams$: Observable<any> = this.http.get('/api/teams');
+	constructor(private http: HttpClient) { }
+}
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+	  BrowserModule, HttpClientModule, CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
